16fd4ee042efed9c38298a3a5f0ce340249e7481
Bianca <bianca03constandache@gmail.com>
2026-05-07 20:45:21 +0300

    Add:demo stil butoane

--- protectia-catodica/src/pages/Experiment.jsx
+++ protectia-catodica/src/pages/Experiment.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Footer from '../site/Footer';
 import Components from '../components/Components.jsx';
-import Formulas from '../components/Formulas.jsx';
-
 
 function Experiment() {
 
