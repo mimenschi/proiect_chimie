8a3657101773c750e8389b298bcd1918d0e5a25f
George Cijenschi <george.cijenschi2602@gmail.com>
2026-05-07 22:11:52 +0300

    Add: Reactii teorie

--- protectia-catodica/src/pages/Teorie.jsx
+++ protectia-catodica/src/pages/Teorie.jsx
@@ -5,12 +5,60 @@ import Footer from '../site/Footer'
 function Teorie() {
   return(
     <>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+      <h2>Reactii chimie:</h2>
+      <div>
+        <h3 className='textTeorie'>1. Coroziunea cu depolarizare de hidrogen</h3>
+        <div className='textReactii'>
+          <strong>In medii acide:</strong>
+          <pre>
+            Oxidare: Fe → Fe²⁺ + 2e⁻
+          </pre>
+          <pre>
+            Reducere: 2H⁺ + 2e⁻ → H₂
+          </pre>
+          <pre>
+            Reacție globală: Fe + 2H⁺ → Fe²⁺ + H₂
+          </pre>
+
+          <strong>In medii neutre si alcaline:</strong>
+          <pre>
+            Oxidare: Fe → Fe²⁺ + 2e⁻
+          </pre>
+          <pre>
+            Reducere: 2H₂O + 2e⁻ → 2OH⁻ + H₂
+          </pre>
+          <pre>
+            Reacție globală: Fe + 2H₂O → Fe²⁺ + 2OH⁻ + H₂
+          </pre>
+        </div>
+      </div>
+
+      <div>
+        <h3 className='textTeorie'>2. Coroziunea cu depolarizare de oxigen</h3>
+        <div className='textReactii'>
+          <strong>In medii acide:</strong>
+          <pre>
+            Oxidare: Fe → Fe²⁺ + 2e⁻
+          </pre>
+          <pre>
+            Reducere: 2H⁺ + ½O₂ + 2e⁻ → H₂O
+          </pre>
+          <pre>
+            Reacție globală: Fe + 2H⁺ + ½O₂ → Fe²⁺ + H₂O
+          </pre>
+
+          <strong>In medii neutre si alcaline:</strong>
+          <pre>
+            Oxidare: Fe → Fe²⁺ + 2e⁻
+          </pre>
+          <pre>
+            Reducere: H₂O + ½O₂ + 2e⁻ → 2OH⁻
+          </pre>
+          <pre>
+            Reacție globală: Fe + H₂O + ½O₂ → Fe(OH)₂
+          </pre>
+        </div>
+      </div>
       <Footer />
     </>
   );
