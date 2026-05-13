28a4102c9a523a221095a8236a223691596d26aa
Bianca <bianca03constandache@gmail.com>
2026-05-13 22:48:40 +0300

    Add:quiz gata

--- protectia-catodica/src/components/Quiz.jsx
+++ protectia-catodica/src/components/Quiz.jsx
@@ -1,13 +1,5 @@
 import React, { useState, useEffect } from "react"
 function Quiz() {
-
-    const [questions, setQuestions] = useState([]);
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [score, setScore] = useState(0);
-    const [finish, setFinish] = useState(false);
-    const [selectedAnswer, setSelectedAnswer] = useState(null);
-    const [answered, setAnswered] = useState(false);
-
     //intrebarile
     const questionsData = [
         {
@@ -16,7 +8,7 @@ function Quiz() {
                 { text: "Acoperirea metalului cu un strat izolator care să împiedice contactul cu oxigenul", isCorrect: false },
                 { text: "Creșterea potențialului de coroziune spre valori pozitive pentru a stabiliza metalul", isCorrect: false },
                 { text: "Folosirea unui curent extern pentru a neutraliza ionii de hidrogen din mediu", isCorrect: false },
-                {text:"Modificarea potențialului de coroziune spre valori negative astfel încât coroziunea să nu mai aibă loc", isCorrect:true}
+                { text: "Modificarea potențialului de coroziune spre valori negative astfel încât coroziunea să nu mai aibă loc", isCorrect: true }
             ],
             correctAnswer: 3,
         },
@@ -25,9 +17,10 @@ function Quiz() {
             options: [
                 { text: "H₂0", isCorrect: true },
                 { text: "H₂", isCorrect: false },
-                { text: "M(OH)₂", isCorrect: false }
+                { text: "M(OH)ₓ", isCorrect: true },
+                { text: "săruri", isCorrect: false }
             ],
-            correctAnswer: 0,
+            correctAnswer: 2,
         },
         {
             question: "Ce trebuie făcut cu plăcuțele de metal înainte de fiecare măsurătoare?",
@@ -49,7 +42,34 @@ function Quiz() {
             ],
             correctAnswer: 0,
         },
+        {
+            question: "Care este rolul anozilor de sacrificiu într-un sistem de protecție?",
+            options: [
+                { text: "Să se coordoneze ei înșiși pentru a proteja metalul principal", isCorrect: true },
+                { text: "Să mărească viteza de coroziune a fierului", isCorrect: false },
+                { text: "Să schimbe culoarea soluției de electrolit", isCorrect: false },
+                { text: "Să respingă ionii de oxigen", isCorrect: false }
+            ],
+            correctAnswer: 0,
+        },
+        {
+            question: "În ce stare trebuie să fie suprafața metalelor pentru a asigura un contact electric bun?",
+            options: [
+                { text: "Curată, fără impurități", isCorrect: true },
+                { text: "Acoperită cu un strat de rugină", isCorrect: false },
+                { text: "Unsă cu ulei", isCorrect: false },
+                { text: "Nu contează", isCorrect: false }
+            ],
+            correctAnswer: 0,
+        },
     ];
+
+    const [questions, setQuestions] = useState(questionsData);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [answered, setAnswered] = useState(false);
+    const [finish, setFinish] = useState(false);
 
     //amestecarea vectorului - fisher-yates
     const shuffle = (array) => {
@@ -60,57 +80,6 @@ function Quiz() {
         }
         return newarray;
     };
-
-    //amestecarea intrebarilor si a raspunsurilor la inceput de quiz
-    //+setarea starilor caracteristice inceputului de quiz
-    useEffect(() => {
-        //amestecarea optiunilor
-        const afterQuestions = questionsData.map((question) => ({
-            ...question,
-            options: shuffle(question.options)
-        }));
-
-        //amestecarea intrebarilor
-        setQuestions(shuffle(afterQuestions));
-
-        //starea 0 a quizului
-        setCurrentQuestion(0);
-        setScore(0);
-        setAnswered(false);
-        setSelectedAnswer(null);
-        setFinish(false);
-    }, []);
-
-    //cand se apasa un raspuns
-    const handleAnswer = (isCorrect, index) => {
-        //se inregistreaza inputul
-        setSelectedAnswer(index);
-        setAnswered(true);
-
-        //se verifica daca inputul este corect
-        if (isCorrect) {
-            //daca inputul este cel corect, se incrementeaza score-ul
-            setScore(score + 1);
-        }
-
-    };
-
-
-    //mergi la urmatoarea intrebare
-    const nextQuestion = () => {
-        const nextQuestion = currentQuestion + 1;
-
-        //daca mai sunt intrebari, se muta la urmatoarea
-        if (nextQuestion < questions.length) {
-            setCurrentQuestion(nextQuestion);
-            setAnswered(false);
-            setSelectedAnswer(null);
-        }
-        //daca nu mai sunt intrebari, se afiseaza score-ul (s-a terminat quizul)
-        else {
-            setFinish(true);
-        }
-    }
 
     //restart quiz
     const handleRestart = () => {
@@ -129,43 +98,83 @@ function Quiz() {
         setFinish(false);
     };
 
+    //amestecarea intrebarilor si a raspunsurilor la inceput de quiz
+    //+setarea starilor caracteristice inceputului de quiz
+    useEffect(() => {
+        handleRestart();
+    }, []);
+
+    //cand se apasa un raspuns
+    const handleAnswer = (isCorrect, index) => {
+        //pentru a evita trisatul
+        if(answered) 
+            return;
+        //se inregistreaza inputul
+        setSelectedAnswer(index);
+        setAnswered(true);
+
+        //se verifica daca inputul este corect
+        if (isCorrect) {
+            //daca inputul este cel corect, se incrementeaza score-ul
+            setScore(score + 1);
+        }
+
+    };
+
+    //mergi la urmatoarea intrebare
+    const nextQuestion = () => {
+        const nextQuestion = currentQuestion + 1;
+
+        //daca mai sunt intrebari, se muta la urmatoarea
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion);
+            setAnswered(false);
+            setSelectedAnswer(null);
+        }
+        //daca nu mai sunt intrebari, se afiseaza score-ul (s-a terminat quizul)
+        else {
+            setFinish(true);
+        }
+    }
+
     return (
-            <div className="quiz">
-                {/*afisarea rezultatelor:output rezultate:intrebarea curenta*/}
-                {finish ? (
-                    <div className="finishedQuiz">
-                        <h2>Test finalizat!</h2>
-                        <div className="score">
-                            <p className="score-text">
-                                Ai obținut {score}/{questionsData.length}!
-                            </p>
-                        </div>
-                        <button className="restart-quiz-button" onClick={handleRestart}>
-                            Repetă testul
-                        </button>
+        <div className="quiz">
+            {/*afisarea rezultatelor:output rezultate:intrebarea curenta*/}
+            {finish ? (
+                <div className="finishedQuiz">
+                    <h2>Test finalizat!</h2>
+                    <div className="score">
+                        <p className="score-text">
+                            Ai obținut {score}/{questionsData.length}!
+                        </p>
                     </div>
-                    ) : (
-                    < div className="quiz-inprogress">
-                        <div className="question-counter">
+                    <button className="restart-quiz-button" onClick={handleRestart}>
+                        Repetă testul
+                    </button>
+                </div>
+            ) : (
+                < div className="quiz-inprogress">
+                    <div className="question-counter">
                         <h3>Întrebarea {currentQuestion + 1}:</h3>
-                    <h2>{questions[currentQuestion].question}</h2>
-                    <div className="quiz-options">
-                        {questions[currentQuestion].options.map((option, index) =>
-                            (<button 
-                                key={index} onClick={() => handleAnswer(option.isCorrect, index)}
-                                    disabled={answered}>
-                                    <span className="options-quiz">{option.text}</span>
+                        <h2>{questions[currentQuestion].question}</h2>
+                        <div className="quiz-options">
+                            {questions[currentQuestion].options.map((option, index) =>
+                            (<button
+                                key={index}
+                                onClick={() => handleAnswer(option.isCorrect, index)}
+                                disabled={answered}>
+                                <span className="options-quiz">{option.text}</span>
                             </button>
                             ))}
+                        </div>
                     </div>
-                </div>
 
-                {answered && (
-                    <button className="button-next" onClick={"nextQuestion"}>
-                    {currentQuestion == (questions.length - 1) ? "Vezi rezultatul" : "Următoarea întrebare"}
-                </button>)}
-            </div >
-                    )}
+                    {answered && (
+                        <button className="button-next" onClick={nextQuestion}>
+                            {currentQuestion === (questions.length - 1) ? "Vezi rezultatul" : "Următoarea întrebare"}
+                        </button>)}
+                </div >
+            )}
         </div>
     );
 };
--- protectia-catodica/src/pages/Experiment.jsx
+++ protectia-catodica/src/pages/Experiment.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from '../site/Footer';
 import Components from '../components/Components.jsx';
+import Quiz from '../components/Quiz.jsx';
 
 function Experiment() {
 
