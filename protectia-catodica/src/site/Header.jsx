6bfdbfadf31e008e46cfa707e215944f29c947f4
George Cijenschi <george.cijenschi2602@gmail.com>
2026-05-17 14:02:21 +0300

    Add: pagina principala

--- protectia-catodica/src/site/Header.jsx
+++ protectia-catodica/src/site/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Experiment from '../pages/Experiment.jsx'
 import Teorie from '../pages/Teorie.jsx'
 import '../pages/TeorieStyle.css'
+import CPU from "../assets/CPU.png"
+import PB from "../assets/motherboard.png"
 
 
 function Header() {
@@ -21,18 +23,32 @@ function Header() {
             în evidenţă ce metale pot fi utilizate ca anozi de sacrificiu.
             </p>
             <h1 className="titlu">Motivație:</h1>
-            <p className="textTeorie fade-in-on-load"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <p className="textTeorie fade-in-on-load"> Coroziunea electrică se referă la procesul de deteriorare a dispozitivelor electrice, ca urmare a reacțiilor chimice cu mediul în care se află.
             </p>
             <br />
             <p className="textTeorie fade-in-on-load">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Acest fenomen reprezintă un factor ridicat de risc într-un sistem de calcul, deoarece poate cauza diferite componente să funcționeze necorespunzător sau să se oprească din funcționare. 
+            De asemenea, fenomenul poate reduce sever durata de viață a componentelor afectate. Deși coroziunea poate fi curățată de pe suprafața componentelor, prevenirea acesteia este mult mai benefică,
+            deoarece costurile pot ajunge foarte sus pentru aceasta.
             </p>
+            <p className="textTeorie fade-in-on-load">
+            Fiind studenți la o facultate din domeniul electric suntem profund afectați de acest fenomen în viața de zi cu zi. 
+            Simularea acestui fenomen ne poate ajuta să determinăm durata de viață a unui circuit și ce 
+            putem face pentru a-l menține funcțional cât mai mult.
+            </p>
+
+             <div className="poze">
+                 <img className="pozaCPU fade-in-on-load"
+                     src={CPU}
+                     alt="CPU"
+
+                 />
+                 <img className="pozaCPU fade-in-on-load"
+                     src={PB}
+                     alt="Motherboard"
+
+                 />
+             </div>
         </header>
     );
 }
